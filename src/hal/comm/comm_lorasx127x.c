458736b60a8c03366c8a5a35605191334bc52452
Vitor Barros <vba@cesar.org.br>
2017-08-01 14:03:44 -0300

    LoRa: Add msg structs
    This patch add a struct for mgmt and data transmission msg.

--- src/hal/comm/comm_lorasx127x.c
+++ src/hal/comm/comm_lorasx127x.c
@@ -8,6 +8,37 @@
  */
 
 #include "include/comm.h"
+#include "hal/lorasx127x.h"
+
+#define MGMT_SIZE		60
+#define DATA_SIZE		60
+
+/* GW address */
+static struct lora_mac mac_local = {.address.uint64 = 0};
+
+struct lora_mgmt {
+	uint8_t buffer_tx[MGMT_SIZE];
+	size_t len_tx;
+};
+
+static struct lora_mgmt mgmt = {.len_tx = 0};
+
+struct lora_data{
+	int8_t sock;
+	uint8_t buffer_tx[DATA_SIZE];
+	size_t len_tx;
+	uint8_t seqnumber_tx;
+	struct lora_mac mac;
+};
+
+#ifndef ARDUINO	/* If master then 5 peers */
+#define MAX_PEERS		255
+#else	/* If slave then 1 peer */
+#define MAX_PEERS		1
+#endif
+
+static struct lora_data peers[MAX_PEERS];
+
 
 int hal_comm_init(const char *pathname, void *params)
 {
